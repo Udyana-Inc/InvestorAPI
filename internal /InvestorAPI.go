0fcaebd13f97e6e9716db571112f9c10990937ce
email.hari.iyer@gmail.com <hariiyer@haris-air.myfiosgateway.com>
2023-10-06 17:48:13 -0400

    b

--- internal /InvestorAPI.go
+++ internal /InvestorAPI.go
@@ -1,34 +1,34 @@
-package InvestorAPI
+package Investor
 
-//import (
-//"github.com/Udyana-Inc/InvestorAPI/models"
-//"github.com/jinzhu/copier"
-//)
+import (
+	"github.com/Udyana-Inc/InvestorAPI/internal/models"
+	"github.com/jinzhu/copier"
+)
 
-// func CompoundInterest(req models.InvestorRequest) (res *models.InvestorResponse, err error) {
-// 	interestRate := (1 + (req.interestRate / 100))
-// 	// calculate the compound ending balance after taxes or regardless of taxes
-// 	// copy over similar values from request to response
-// 	err = copier.Copy(res, &req)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if req.afterTax == false {
-// 		for years := 0; years <= req.yearsHeld; years++ {
-// 			//non taxable account
-// 			res.endingBalance = interestRate * req.beginningBalance
-// 		}
+func CompoundInterest(req models.InvestorRequest) (res *models.InvestorResponse, err error) {
+	interestRate := (1 + (req.interestRate / 100))
+	// calculate the compound ending balance after taxes or regardless of taxes
+	// copy over similar values from request to response
+	err = copier.Copy(res, &req)
+	if err != nil {
+		return nil, err
+	}
+	if req.afterTax == false {
+		for years := 0; years <= req.yearsHeld; years++ {
+			//non taxable account
+			res.endingBalance = interestRate * req.beginningBalance
+		}
 
-// 	} else if req.afterTax == true {
-// 		taxRate := 1 - (req.taxRate / 100)
-// 		//taxable account
-// 		for years := 0; years <= req.yearsHeld; years++ {
-// 			//non taxable account
-// 			res.endingBalance = interestRate * (req.beginningBalance) * taxRate
-// 		}
+	} else if req.afterTax == true {
+		taxRate := 1 - (req.taxRate / 100)
+		//taxable account
+		for years := 0; years <= req.yearsHeld; years++ {
+			//non taxable account
+			res.endingBalance = interestRate * (req.beginningBalance) * taxRate
+		}
 
-// 	}
+	}
 
-// 	return
+	return
 
-// }
+}
